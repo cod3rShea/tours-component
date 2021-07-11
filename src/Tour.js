ed2634d885937eaaf53a294d18768577df8ee8de
Sean Michael Shea <seanshea1337@gmail.com>
2021-07-11 09:35:04 -0400

    render passed props from parent  component

--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name }) => {
+	console.log(id);
+	return (
+		<>
+			<article className="single-tour">
+				<img src={image} alt={name} />
+				<footer>
+					<div className="tour-info">
+						<h4>{name}</h4>
+						<h4 className="tour-price">${price}</h4>
+					</div>
+					<p>
+						{info}
+						<button>read more</button>
+					</p>
+					<button className="delete-btn">not interested</button>
+				</footer>
+			</article>
+		</>
+	);
 };
 
 export default Tour;
